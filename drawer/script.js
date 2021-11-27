211f503a4d3e4ce9ddb9dc86e35b7dbc1fe53e2d
hamilsauce <jake@seahag.onmicrosoft.com>
2021-11-27 13:22:43 -0600

    hik

--- drawer/script.js
+++ drawer/script.js
@@ -1,44 +1,43 @@
 export class Drawer {
   constructor(parentSelector = '#app') {
+    this.parent = document.querySelector(parentSelector);
     this.drawer = document.querySelector('#drawer-template').content.firstElementChild.cloneNode(true) || document.createElement('div')
     this.handle = this.drawer.querySelector('.drawer-handle');
     console.log('this.drawer.tagName', this.drawer.tagName)
-    this.init(parentSelector);
+    this.currentHeight
+    this.init();
 
   }
 
-  init(parentSelector) {
-    document.querySelector(parentSelector).appendChild(this.drawer)
+  get currentDrawerHeight() {
+    return parseInt(getComputedStyle(this.drawer).height)
+  }
+
+  init() {
+    this.parent.appendChild(this.drawer)
     this.handle.addEventListener('dblclick', this.doubleClickDrawerHandle.bind(this));
-    document.addEventListener('click', e => {
-      // console.log('this.testEventPath(e, this.drawer, this.handle)', this.testEventPath(e, this.drawer, this.handle))
-      // console.log('e.target.classList.contains(frawer)) {', !e.target.classList.contains('drawer'))
-      if (
-        !e.target.classList.contains('drawer')
-      ) {
+
+    this.parent.addEventListener('click', e => {
+      const pathContains = [...e.path].some(_ => _ === this.drawer)
+      if (!pathContains) {
         this.drawer.style.transition = '0.6s ease-in-out';
-        this.drawer.style.height = `${120}px`;
+        this.drawer.style.height = `${136}px`;
         this.drawer.dataset.expanded = 'false';
         setTimeout(() => this.drawer.style.transition = '', 600)
       }
     });
     this.handle.addEventListener('touchstart', this.startDrag.bind(this));
-    // document.addEventListener('touchstart', this.startDrag.bind(this));
+    document.addEventListener('touchstart', this.startDrag.bind(this));
   }
 
-  isHandleEventSource(e) { return e.path.some(el => el.id === 'drawer-handle') }
-
-  testEventPath(e, ...elements) {
-    console.log('[path, elements]', [e.composedPath(), elements])
-    return e.composedPath().reduce((matched, eventEl, i) => {
-      console.log('matched, eventEl, elements', [matched, eventEl, elements]);
-      return elements.some(el => el == eventEl)
-    }, false);
-  }
+  isHandleEventSource(e) { return [...e.path].some(el => el === this.handle) }
 
   startDrag(e) {
+    console.log('this.isHandleEventSource(e)', this.isHandleEventSource(e))
     if (this.isHandleEventSource(e)) {
+
       this.handle.classList.add('pressed');
+    
       this.handle.addEventListener('touchmove', this.dragDrawer.bind(this), true)
       this.handle.addEventListener('touchend', this.stopDrag.bind(this), true)
       e.preventDefault();
@@ -55,10 +54,10 @@ export class Drawer {
     const currentHeight = parseInt(getComputedStyle(this.drawer).height)
     const maxHeight = 450;
     const appHeight = parseInt(getComputedStyle(document.body).height)
-    const touch = e.changedTouches[0].pageY
+    const touch = e.changedTouches[0].pageY - 10;
 
     if ((touch > (appHeight - 100))) return;
-    else if (touch <= (appHeight - maxHeight)) this.drawer.style.height = `${currentHeight}px`;
+    else if (touch <= (appHeight - maxHeight)) this.drawer.style.height = `${this.currentDrawerHeight}px`;
     else this.drawer.style.height = `${(appHeight - touch)}px`;
   }
 
@@ -72,6 +71,10 @@ export class Drawer {
     }
   }
 
+  toggleDrawerTransitionCSS() {
+    this.drawer.style.transition = '0.6s ease-in-out';
+    setTimeout(() => this.drawer.style.transition = '', 600)
+  }
   handleClick(e) {
     this.drawer.dispatch(new CustomEvent('draw-clicked'))
   }
